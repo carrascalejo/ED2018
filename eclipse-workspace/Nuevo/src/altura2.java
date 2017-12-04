a6ef3d58a50c41445b781c500e46228640eaf401
Your Name <you@example.com>
2017-12-04 18:05:32 +0100

    Prueba

---
+++ eclipse-workspace/Nuevo/src/altura2.java
@@ -0,0 +1,19 @@
+
+public class altura2 {
+	
+static int altura4=0;
+
+public static int getAltura() {
+	return altura4;
+}
+
+
+public static void main (String[] arg)
+{
+	System.out.println("Hola");
+	System.out.println("Adios");
+	
+	
+}
+	
+}
